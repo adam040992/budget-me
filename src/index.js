0d4b3f5a0631330b7acb65f45b00bb964704f59a
adam040992 <adam040992@gmail.com>
2021-02-23 19:46:31 +0100

    create store and reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import './i18n/i18n';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from 'data/store';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
