d531115d8dd93295d281d238edba17fd5b83a82a
adam040992 <adam040992@gmail.com>
2021-03-03 17:45:36 +0100

    Lodash installed

--- src/pages/Budget/components/BudgetCategoryList/BudgetCategoryList.js
+++ src/pages/Budget/components/BudgetCategoryList/BudgetCategoryList.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import { groupBy } from 'lodash'
 
-const BudgetCategoryList = ({  }) => {
+const BudgetCategoryList = ({ budgetedCategories, allCategories }) => {
+    const budgetedCategoriesByParent = groupBy(budgetedCategories, item => allCategories.find(category => category.id === item.categoryId).parentCategory.name);
 
-    useEffect = {
-        
-    }
-
+    console.log(budgetedCategories);
+    console.log(budgetedCategoriesByParent);
     return (
         <div>BudgetCategoryList</div>
     )
 }
 
-export default BudgetCategoryList;
+export default connect(state = ({
+    budgetedCategories: state.budget.budgetedCategories,
+    allCategories: state.common.allCategories,
+}))(BudgetCategoryList);
