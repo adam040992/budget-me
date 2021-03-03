6fba6d0c34392c1d4743a44fac7f50d4b85a7ae6
adam040992 <adam040992@gmail.com>
2021-03-03 17:32:07 +0100

    added BudgetedCategoryList

---
+++ src/pages/Budget/components/BudgetCategoryList/BudgetCategoryList.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+const BudgetCategoryList = ({  }) => {
+
+    useEffect = {
+        
+    }
+
+    return (
+        <div>BudgetCategoryList</div>
+    )
+}
+
+export default BudgetCategoryList;
